1fe738e7ffaaa74d2f97d6c8d12aac9675f6338a
Théo Gianella <theo.gianella@proton.me>
2025-01-11 14:34:12 +0100

    use deployed demo apps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://tgianella.github.io",
-	base: "/talk-wiew-transitions",
+	base: "/talk-view-transitions",
 });
