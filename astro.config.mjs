49b9015e5b526bb720d9600f4de7040c273ae209
Théo Gianella <theo.gianella@proton.me>
2025-01-11 12:54:18 +0100

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jsulpis.github.io",
-	base: "/slides-template",
+	site: "https://tgianella.github.io",
+	base: "/talk-wiew-transitions",
 });
